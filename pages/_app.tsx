08b98dd2dfc5550a9af278cdeadb1d411b229b84
Ulisseus <newdaddy1911@gmail.com>
2021-07-25 21:13:15 +0300

    add globalStyle

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,12 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { store } from "../store/store";
+import GlobalStyle from "../styles/globalStyle";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <GlobalStyle />
       <Component {...pageProps} />
     </Provider>
   );
