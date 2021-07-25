99a9f0e84f4cb687ea460ba35645f5811c0ad5ec
Ulisseus <newdaddy1911@gmail.com>
2021-07-25 18:09:00 +0300

    restyle navBar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,48 @@
 import React from "react";
 import styled from "styled-components";
 
+const StyledNav = styled.nav`
+  padding: 0.5em 3em;
+  display: flex;
+  align-items: center;
+  & > h1 {
+    font-family: "ultra";
+    font-size: 3rem;
+    @font-face {
+      font-family: "ultra";
+      src: url("/Ultra-Regular.ttf");
+      font-style: normal;
+      font-weight: 400;
+      font-display: swap;
+    }
+  }
+  & > ul {
+    margin-left: auto;
+  }
+  & > ul > li {
+    display: inline;
+    cursor: pointer;
+    &:hover {
+      color: #6b6b6b;
+    }
+  }
+`;
+
+const NavButton = styled.li`
+  font-weight: bold;
+  font-size: 1.5rem;
+  margin: 0 1em;
+`;
+
 const NavBar: React.FC = () => {
   return (
-    <nav>
+    <StyledNav>
       <h1>A simple blog</h1>
-    </nav>
+      <ul>
+        <NavButton>Posts</NavButton>
+        <NavButton>Create Post</NavButton>
+      </ul>
+    </StyledNav>
   );
 };
 
--- components/Post.tsx
+++ components/Post.tsx
@@ -4,6 +4,7 @@ import { Post as PostType } from "../types/post";
 import { useRouter } from "next/router";
 
 const StyledPost = styled.div`
+  padding: 0.5em 1em;
   display: flex;
   flex-direction: column;
   text-align: center;
@@ -11,14 +12,27 @@ const StyledPost = styled.div`
   background-color: white;
   border: 1px solid black;
   margin: 1em 2em;
+  cursor: pointer;
+`;
+
+const PostTitle = styled.b`
+  font-size: 1.5em;
+`;
+
+const PostBody = styled.section`
+  margin: 1em auto;
+  color: #797979;
+  height: 100%;
+  display: flex;
+  align-items: center;
 `;
 
 const Post: React.FC<{ post: PostType }> = ({ post }) => {
   const router = useRouter();
   return (
     <StyledPost onClick={() => router.push(`/post/${post.id}`)}>
-      <b>{post.title}</b>
-      <section>{post.body}</section>
+      <PostTitle>{post.title}</PostTitle>
+      <PostBody>{post.body}</PostBody>
     </StyledPost>
   );
 };
