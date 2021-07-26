972a4aff8bca200e3f7f30f775cfd78ba3e37454
Ulisseus <newdaddy1911@gmail.com>
2021-07-26 12:09:14 +0300

    add title to blog

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,14 @@ import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { store } from "../store/store";
 import GlobalStyle from "../styles/globalStyle";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <GlobalStyle />
+      <meta name="title" content="A simple blog" />
+      <title key="title">A simple blog</title>
       <Component {...pageProps} />
     </Provider>
   );
